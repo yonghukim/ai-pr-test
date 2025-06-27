349c6b9566f5e53fd0507e57a835296afbf4a4e4
yonghukim <yonghu.kim@kurlycorp.com>
2025-06-27 18:41:33 +0900

    테스트용 enum 추가

---
+++ src/main/java/com/example/demo/service/LongContextTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+public enum LongContextTest {
+    Target,
+    TEST2,
+    TEST3,
+    TEST4,
+    TEST5,
+    TEST6,
+    TEST7,
+    TEST8,
+    TEST9,
+    TEST10,
+    TEST11,
+    TEST12,
+    TEST13,
+    TEST14,
+    TEST15,
+    TEST16,
+    TEST17,
+    TEST18,
+    TEST19,
+    TEST20,
+    TEST21,
+    TEST22,
+    TEST23,
+    TEST24,
+    TEST25,
+    ;
+}
